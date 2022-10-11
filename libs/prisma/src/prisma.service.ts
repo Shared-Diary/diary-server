a13030a390f8f3f04399d196fde47bac986dcf29
choidoorim <doorim321@naver.com>
2022-10-11 23:22:12 +0900

    ci: monorepo 방식으로 변경 및 script 추가

---
+++ libs/prisma/src/prisma.service.ts
@@ -0,0 +1,32 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { Prisma, PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    super({
+      log: [
+        { emit: 'stdout', level: 'query' },
+        { emit: 'stdout', level: 'info' },
+        { emit: 'stdout', level: 'warn' },
+        { emit: 'stdout', level: 'error' },
+      ],
+    });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+
+  static isPrismaError = (
+    error: unknown,
+  ): error is Prisma.PrismaClientKnownRequestError =>
+    error instanceof Prisma.PrismaClientKnownRequestError;
+}
