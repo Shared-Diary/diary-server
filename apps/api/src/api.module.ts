aa3165f73da6ae1c4f7033d94e17e9bb737abf82
choidoorim <doorim321@naver.com>
2022-10-13 23:23:16 +0900

    chore: swagger install

--- apps/api/src/api.module.ts
+++ apps/api/src/api.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { ApiController } from './api.controller';
 import { ApiService } from './api.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env`],
+      isGlobal: true,
+    }),
+  ],
   controllers: [ApiController],
   providers: [ApiService],
 })
