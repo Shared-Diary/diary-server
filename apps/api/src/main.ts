24ce1e0edd366ba886de56fa8be77a49edc89e9e
choidoorim <doorim321@naver.com>
2022-11-08 23:25:58 +0900

    feat: dev 환경에서만 swagger 참고가능하도록 변경

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,16 +3,20 @@ import { NestFactory } from '@nestjs/core';
 import { ApiModule } from './api.module';
 import { corsOptions, buildSwagger } from './configs';
 
+const isDevelopment = ['local', 'dev'].includes(process.env.NODE_ENV);
+
 async function bootstrap() {
   const app = await NestFactory.create(ApiModule);
   app.enableVersioning();
 
   app.enableCors(corsOptions);
-  buildSwagger(app, {
-    title: 'Diary Server',
-    description: 'Diary Server API 입니다',
-    path: '/api',
-  });
+  if (isDevelopment) {
+    buildSwagger(app, {
+      title: 'Diary Server',
+      description: 'Diary Server API 입니다',
+      path: '/api',
+    });
+  }
 
   await app.listen(process.env.PORT || 3000);
 }
