45b6a2c5d184cb72c1bec4c8fd6834f441d47578
choidoorim <doorim321@naver.com>
2022-11-23 02:07:38 +0900

    feat: swagger

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,20 +3,20 @@ import { NestFactory } from '@nestjs/core';
 import { ApiModule } from './api.module';
 import { corsOptions, buildSwagger } from './configs';
 
-const isDevelopment = ['local', 'dev'].includes(process.env.NODE_ENV);
+const isDevelopment = ['local', 'dev', 'prod'].includes(process.env.NODE_ENV);
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiModule);
   app.enableVersioning();
 
   app.enableCors(corsOptions);
-  // if (isDevelopment) {
-  buildSwagger(app, {
-    title: 'Diary Server',
-    description: 'Diary Server API 입니다',
-    path: '/api',
-  });
-  // }
+  if (isDevelopment) {
+    buildSwagger(app, {
+      title: 'Diary Server',
+      description: 'Diary Server API 입니다',
+      path: '/api',
+    });
+  }
 
   await app.listen(process.env.PORT || 3000);
 }
