51fd0be413bcd4126bd4b4209aa9326fc92da4c7
choidoorim <doorim321@naver.com>
2022-11-23 23:59:36 +0900

    ci: fix

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -3,10 +3,8 @@ REPOSITORY=/home/ubuntu/diary-server
 
 cd $REPOSITORY || exit
 
-sudo npm run build
+npm run prisma:generate
 
-sudo npm run prisma:generate
-
-sudo npm run pm2:start:prod
+npm run pm2:start:prod
 
 echo "success!!"
