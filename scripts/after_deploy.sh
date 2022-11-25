11f3c8290a85b0b0e688b73f7f13f2d1d9e5cbd4
choidoorim <doorim321@naver.com>
2022-11-26 01:54:06 +0900

    ci: test

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-REPOSITORY=/home/ec2-user/diary-server
+REPOSITORY=/home/ec2-user/build
 
 cd $REPOSITORY
 
 sudo npm run prisma:generate
 
-sudo npm run pm2:restart
+sudo pm2 reload app
 
 echo "success!!"
-
-# npm 등 명령이 제대로 수행되고 있지 않는 것은 sudo 명령 문제를 해결하면 될 수도 있겠다
